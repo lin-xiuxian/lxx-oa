1b8e061ecd40cfd61c6d57ebf4787a39fb2cae84
lin-xiuxian <2625453652@qq.com>
2023-02-04 01:53:36 +0800

    不同角色创建多级请假流程 service 层

--- src/main/java/com/lxx/oa/service/LeaveFormService.java
+++ src/main/java/com/lxx/oa/service/LeaveFormService.java
@@ -16,13 +16,14 @@ import java.util.Date;
  * @description 完成多级请假流程的业务逻辑
  */
 public class LeaveFormService {
+    private EmployeeService employeeService = new EmployeeService();
     /**
      * 创建请假单
      * @param form 前端输入的请假单据
      * @return 持久化之后的请假单对象
      */
     public LeaveForm createLeaveForm(LeaveForm form) {
-        MybatisUtils.executeUpdate(sqlSession -> {
+        LeaveForm f = (LeaveForm) MybatisUtils.executeUpdate(sqlSession -> {
             //1. 持久化form表单数据，8级一下员工表单状态为 processing, 8级（总经理）状态为 approved
             EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
             Employee employee = employeeMapper.selectById(form.getEmployeeId());
@@ -46,10 +47,63 @@ public class LeaveFormService {
             processFlowMapper.insert(flow1);
             //3. 分情况创建其余流程数据
             //3.1 7级一下员工，生成 部门 经理审批任务，请假时间大于 72 小时，还需生成总经理审批任务
-            //3.2 7级员工，仅生成总经理审批任务
-            //3.3 8级员工，生成总经理审批任务，系统自动通过
-            return null;
+            if(employee.getLevel() < 7){
+                Employee dmenager = employeeService.selectLeader(employee.getEmployeeId());
+                ProcessFlow flow2 = new ProcessFlow();
+                flow2.setFormId(form.getFormId());
+                flow2.setOperatorId(dmenager.getEmployeeId());
+                flow2.setAction("audit");
+                flow2.setCreateTime(new Date());
+                flow2.setOrderNo(2);
+                flow2.setState("process");
+                long diff = form.getEndTime().getTime() - form.getStartTime().getTime();
+                float hours = diff/(1000 * 60 * 60) * 1f;
+                if(hours > 72){
+                    flow2.setIsLast(0);
+                    processFlowMapper.insert(flow2);
+                    Employee manager = employeeService.selectLeader(dmenager.getEmployeeId());
+                    ProcessFlow flow3 = new ProcessFlow();
+                    flow3.setFormId(form.getFormId());
+                    flow3.setOperatorId(manager.getEmployeeId());
+                    flow3.setAction("audit");
+                    flow3.setCreateTime(new Date());
+                    flow3.setState("ready");
+                    flow3.setOrderNo(3);
+                    flow3.setIsLast(1);
+                    processFlowMapper.insert(flow3);
+                } else {
+                    flow2.setIsLast(1);
+                    processFlowMapper.insert(flow2);
+                }
+            } else if (employee.getLevel() == 7){
+                //3.2 7级员工，仅生成总经理审批任务
+                Employee manager = employeeService.selectLeader(employee.getEmployeeId());
+                ProcessFlow flow2 = new ProcessFlow();
+                flow2.setFormId(form.getFormId());
+                flow2.setOperatorId(manager.getEmployeeId());
+                flow2.setAction("audit");
+                flow2.setCreateTime(new Date());
+                flow2.setState("process");
+                flow2.setOrderNo(2);
+                flow2.setIsLast(1);
+                processFlowMapper.insert(flow2);
+            } else if (employee.getLevel() == 8){
+                //3.3 8级员工，生成总经理审批任务，系统自动通过
+                ProcessFlow flow2 = new ProcessFlow();
+                flow2.setFormId(form.getFormId());
+                flow2.setOperatorId(employee.getEmployeeId());
+                flow2.setAction("audit");
+                flow2.setResult("approved");
+                flow2.setReason("自动通过");
+                flow2.setCreateTime(new Date());
+                flow2.setAuditTime(new Date());
+                flow2.setState("complete");
+                flow2.setOrderNo(2);
+                flow2.setIsLast(1);
+                processFlowMapper.insert(flow2);
+            }
+            return form;
         });
-        return null;
+        return f;
     }
 }
---
+++ src/test/java/com/lxx/oa/service/LeaveFormServiceTest.java
@@ -0,0 +1,91 @@
+package com.lxx.oa.service;
+
+import com.lxx.oa.entity.LeaveForm;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 林修贤
+ * @date 2023/2/4
+ * @description
+ */
+public class LeaveFormServiceTest {
+    private LeaveFormService leaveFormService = new LeaveFormService();
+    /**
+     * 市场部员工请假单(72小时以上)测试用例
+     * @throws ParseException
+     */
+    @Test
+    public void createLeaveForm1() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH");
+        LeaveForm form = new LeaveForm();
+        form.setEmployeeId(8l);
+        form.setStartTime(sdf.parse("2020032608"));
+        form.setEndTime(sdf.parse("2020040118"));
+        form.setFormType(1);
+        form.setReason("市场部员工请假单(72小时以上)");
+        form.setCreateTime(new Date());
+        LeaveForm savedForm = leaveFormService.createLeaveForm(form);
+        System.out.println(savedForm.getFormId());
+    }
+
+    /**
+     * 市场部员工请假单(72小时内)测试用例
+     * @throws ParseException
+     */
+    @Test
+    public void createLeaveForm2() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH");
+        LeaveForm form = new LeaveForm();
+        form.setEmployeeId(8l);
+        form.setStartTime(sdf.parse("2020032608"));
+        form.setEndTime(sdf.parse("2020032718"));
+        form.setFormType(1);
+        form.setReason("市场部员工请假单(72小时内)");
+        form.setCreateTime(new Date());
+        LeaveForm savedForm = leaveFormService.createLeaveForm(form);
+        System.out.println(savedForm.getFormId());
+    }
+
+    /**
+     * 研发部部门经理请假单测试用例
+     * @throws ParseException
+     */
+    @Test
+    public void createLeaveForm3() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH");
+        LeaveForm form = new LeaveForm();
+        form.setEmployeeId(2l);
+        form.setStartTime(sdf.parse("2020032608"));
+        form.setEndTime(sdf.parse("2020040118"));
+        form.setFormType(1);
+        form.setReason("研发部部门经理请假单");
+        form.setCreateTime(new Date());
+        LeaveForm savedForm = leaveFormService.createLeaveForm(form);
+        System.out.println(savedForm.getFormId());
+    }
+
+    /**
+     * 总经理请假单测试用例
+     * @throws ParseException
+     */
+    @Test
+    public void createLeaveForm4() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH");
+        LeaveForm form = new LeaveForm();
+        form.setEmployeeId(1l);
+        form.setStartTime(sdf.parse("2020032608"));
+        form.setEndTime(sdf.parse("2020040118"));
+        form.setFormType(1);
+        form.setReason("总经理请假单");
+        form.setCreateTime(new Date());
+        LeaveForm savedForm = leaveFormService.createLeaveForm(form);
+        System.out.println(savedForm.getFormId());
+    }
+
+}
