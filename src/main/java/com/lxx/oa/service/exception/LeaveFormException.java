2823ca36692c3add4b104948993d3fa10b960ac6
lin-xiuxian <2625453652@qq.com>
2023-02-04 19:50:56 +0800

    完成请假审批的service层

---
+++ src/main/java/com/lxx/oa/service/exception/LeaveFormException.java
@@ -0,0 +1,12 @@
+package com.lxx.oa.service.exception;
+
+/**
+ * @author 林修贤
+ * @date 2023/2/4
+ * @description
+ */
+public class LeaveFormException extends RuntimeException {
+    public LeaveFormException(String message){
+        super(message);
+    }
+}
